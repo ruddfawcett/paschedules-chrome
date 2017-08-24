b0fbd40852405885d6d405bda3dc508ca2b3c6fe
Rudd Fawcett <rudd.fawcett@gmail.com>
2017-08-24 18:23:20 -0400

    "pass" not "password" for connect.php

--- server/connect.php
+++ server/connect.php
@@ -1,7 +1,7 @@
 <?php
   $dbopts = parse_url(getenv('DATABASE_URL'));
 
-  $db = new PDO('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts['host'].';user='.$dbopts['user'].';password='.$dbopts['password']);
+  $db = new PDO('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts['host'].';user='.$dbopts['user'].';password='.$dbopts['pass']);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
